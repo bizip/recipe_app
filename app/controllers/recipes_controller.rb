2e5e37770d00392b4dbbadd2a7c03189be311582
NatiG <nati.g5684@gmail.com>
2022-09-07 02:02:49 +0300

    Create recipe show

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :delete]
     def index
       @recipes = Recipe.all
     end
@@ -8,13 +9,29 @@ class RecipesController < ApplicationController
     end
 
     def create 
-    @recipes = Recipe.new
+    @recipes = Recipe.new(recipe_params)
     @recipes.user_id = current_user.id
 
     if @recipes.save
         redirect_to recipes_path 
     else
-            render :new
+        render :new
     end
+  end
+
+  def destroy
+    @recipe.destroy
+  end
+
+  def show; end
+
+   private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :preparation_time, :cooking_time, :public)
   end
 end
