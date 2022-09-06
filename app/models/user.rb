2386342b2be004f957a91154ccca0ca5aaca5bda
NatiG <nati.g5684@gmail.com>
2022-09-06 16:23:32 +0300

    Create a recipe controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   #  :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+        
+  has_many :recipes, foreign_key: "user_id"
 end
