cf5bce5a46c042b49907ffdfae03b001727f0c27
NatiG <nati.g5684@gmail.com>
2022-09-06 23:18:39 +0300

    Create recipe create method

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,20 @@
 class RecipesController < ApplicationController
-    def index; end
+    def index
+      @recipes = Recipe.all
+    end
 
     def new 
         @recipes = Recipe.new
     end
+
+    def create 
+    @recipes = Recipe.new
+    @recipes.user_id = current_user.id
+
+    if @recipes.save
+        redirect_to recipes_path 
+    else
+            render :new
+    end
   end
+end
