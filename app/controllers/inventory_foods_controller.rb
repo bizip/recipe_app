090cbff85b5cfb373b6d5286d5128bd33f4c641e
HammadSiddique <hammad.siddique01@gmail.com>
2022-09-08 15:06:04 +0500

    Create inventory table and form

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -1,2 +1,11 @@
 class InventoryFoodsController < ApplicationController
+  def destroy
+  @inventory_food = InventoryFood.find_by(id: params[:food_id])
+
+	if @inventory_food.destroy
+		flash[:notice] = 'Inventory food deleted successfully!!'
+	else
+		flash[:alert] = 'Something unexpected happened, inventory could not be deleted.'
+	end
+	redirect_to inventory_path(params[:id])
 end
