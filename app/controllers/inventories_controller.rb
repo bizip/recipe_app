b3e611740de63f4e37d0786581eef76d237b3829
HammadSiddique <hammad.siddique01@gmail.com>
2022-09-08 18:48:31 +0500

    Fix linter errors

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,45 +1,45 @@
 class InventoriesController < ApplicationController
-	def index
-		@user = current_user
-		@inventories = @user.inventories
-	end
+  def index
+    @user = current_user
+    @inventories = @user.inventories
+  end
 
-	def show
-		@user = current_user
-		@inventory = @user.inventories.find(params[:id])
-		@foods = @inventory.inventory_foods.includes(:food)
-	end
+  def show
+    @user = current_user
+    @inventory = @user.inventories.find(params[:id])
+    @foods = @inventory.inventory_foods.includes(:food)
+  end
 
-	def new
-		@inventory = Inventory.new
-	end
+  def new
+    @inventory = Inventory.new
+  end
 
-	def create
-		@inventory = Inventory.create(inventory_params)
-		@inventory.user = current_user
-		if @inventory.save
-			flash[:notice] = 'Inventory created successfully!'
-			redirect_to user_inventories_path
-		else
-			flash.now[:alert] = 'Something unexpected happened, inventory could not be created.'
-			render :new
-		end
-	end
+  def create
+    @inventory = Inventory.create(inventory_params)
+    @inventory.user = current_user
+    if @inventory.save
+      flash[:notice] = 'Inventory created successfully!'
+      redirect_to user_inventories_path
+    else
+      flash.now[:alert] = 'Something unexpected happened, inventory could not be created.'
+      render :new
+    end
+  end
 
-	def destroy
-		@inventory = Inventory.find_by(id: params[:id])
+  def destroy
+    @inventory = Inventory.find_by(id: params[:id])
 
-		if @inventory.destroy
-			flash[:notice] = 'Inventory deleted successfully!'
-		else
-			flash[:alert] = 'Something unexpected happened, inventory could not be deleted.'
-		end
-		redirect_to user_inventories_path
-	end
+    if @inventory.destroy
+      flash[:notice] = 'Inventory deleted successfully!'
+    else
+      flash[:alert] = 'Something unexpected happened, inventory could not be deleted.'
+    end
+    redirect_to user_inventories_path
+  end
 
-	private
+  private
 
-	def inventory_params
-		params.require(:inventory).permit(:name)
-	end
+  def inventory_params
+    params.require(:inventory).permit(:name)
+  end
 end
