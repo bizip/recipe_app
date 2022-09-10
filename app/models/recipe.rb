9d2ab47cf3b3995c5a8ad7d1fcfa053db183dfd6
NatiG <nati.g5684@gmail.com>
2022-09-10 15:15:20 +0300

    Add dependent destroy to inventories & recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,4 @@
 class Recipe < ApplicationRecord
-  has_many :recipe_foods
-  has_many :foods, through: :recipe_foods
-  belongs_to :user
+  has_many :recipe_foods, dependent: :destroy
+  belongs_to :user, foreign_key: 'user_id'
 end
